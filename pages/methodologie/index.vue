83f1043146af5cc39277cfc6672d6c0d7bc09ff5
tomas <tomasmartty@gmail.com>
2024-02-09 20:59:23 +0100

    + working methodologie page

---
+++ pages/methodologie/index.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="relative py-16 bg-white overflow-hidden">
+    <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+      <div class="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+        <svg
+          class="absolute top-12 left-full transform translate-x-32"
+          width="404"
+          height="384"
+          fill="none"
+          viewBox="0 0 404 384"
+        >
+          <defs>
+            <pattern
+              id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+        </svg>
+        <svg
+          class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+          width="404"
+          height="384"
+          fill="none"
+          viewBox="0 0 404 384"
+        >
+          <defs>
+            <pattern
+              id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+        </svg>
+        <svg
+          class="absolute bottom-12 left-full transform translate-x-32"
+          width="404"
+          height="384"
+          fill="none"
+          viewBox="0 0 404 384"
+        >
+          <defs>
+            <pattern
+              id="d3eb07ae-5182-43e6-857d-35c643af9034"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+        </svg>
+      </div>
+    </div>
+    <div class="relative px-4 sm:px-6 lg:px-8">
+      <div class="text-lg max-w-prose mx-auto">
+        <h1>
+          <span
+            class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
+          >
+            Méthodologie
+          </span>
+        </h1>
+      </div>
+      <div class="mt-6 prose prose-lg text-gray-500 mx-auto">
+        <p>
+          Réponses aux questions fréquentes sur les données et la méthodologie.... Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <h2>Lorem Ipsum</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+        <h2>Dolor Sit Amet</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <h2>Réutilisation du contenu du site</h2>
+        <p>
+          Toute réutilisation brute des cartographies interactives du site, des chiffres du tableau de bord et des
+          détails écrits des tronçons de chaque ligne doivent faire systématiquement mention du nom de l’association
+          contributrice « Collectif Vélos en Ville ».
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MentionsLegales'
+};
+</script>
