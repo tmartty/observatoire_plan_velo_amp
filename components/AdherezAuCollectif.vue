f050cdb248947ade60fb6b723abb3ed912ca7aa0
tomas <tomasmartty@gmail.com>
2024-02-09 17:07:03 +0100

    * update donations CTA

---
+++ components/AdherezAuCollectif.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-white">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="py-10 px-6 bg-cvv-blue-600 rounded-3xl sm:py-16 sm:px-12 lg:p-16 lg:flex lg:items-center">
+        <div class="lg:w-0 lg:flex-1 text-center lg:text-left">
+          <h2 class="text-3xl font-extrabold tracking-tight text-white">Adhérez au Collectif Vélos en Ville</h2>
+          <p class="mt-4 max-w-3xl text-lg text-indigo-100">
+            Soutenez la promotion et l'usage du vélo à Marseille, ainsi que le développement d'infrastructures cyclables
+            continues et sécurisées en adhérant au Collectif.
+          </p>
+        </div>
+        <div class="sm:flex justify-center lg:justify-end mt-12 lg:mt-0 lg:ml-8 lg:flex-1">
+          <button
+            type="button"
+            class="mt-3 w-full flex items-center justify-center px-5 py-3 border border-cvv-red text-base font-medium rounded-md text-white bg-cvv-red duration-100 hover:bg-white hover:text-cvv-red sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0"
+          >
+            <a
+              href="https://www.helloasso.com/associations/collectif-velos-en-ville/adhesions/adhesions-au-collectif-velos-en-ville"
+              target="_blank"
+            >
+              Adhérer
+            </a>
+          </button>
+          <button
+            type="button"
+            class="mt-3 w-full flex items-center justify-center px-5 py-3 border border-white text-base font-medium rounded-md text-white bg-transparent duration-100 hover:bg-white hover:text-cvv-red sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0"
+          >
+            <a href="https://www.helloasso.com/associations/collectif-velos-en-ville/formulaires/1" target="_blank">
+              Faire un don
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,10 @@
     <div class="max-w-7xl mx-auto mt-14 px-4 sm:px-6 lg:px-8 lg:mt-24">
       <div class="space-y-8 sm:space-y-12">
         <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl text-center">
-          <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">
-            Où en est le projet ?
-          </h2>
+          <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Où en est le projet ?</h2>
           <p class="text-xl text-gray-500">
-            Après une phase d'étude et de concertation en 2022 et 2023, le projet entre en phase de réalisation.<br> Les travaux devraient se multiplier en 2024 et 2025.
+            Après une phase d'étude et de concertation en 2022 et 2023, le projet entre en phase de réalisation.<br />
+            Les travaux devraient se multiplier en 2024 et 2025.
           </p>
         </div>
       </div>
@@ -19,9 +18,7 @@
     <div class="max-w-7xl mx-auto mt-14 px-4 sm:px-6 lg:px-8 lg:mt-24">
       <div class="space-y-8 sm:space-y-12">
         <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl text-center">
-          <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">
-            Avancement par ligne
-          </h2>
+          <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Avancement par ligne</h2>
           <p class="text-xl text-gray-500">
             Choisissez une Voie Lyonnaise pour connaitre le détail du projet et voir son niveau d'avancement.
           </p>
@@ -30,7 +27,7 @@
       </div>
     </div>
     <div class="py-16">
-      <LvvCta />
+      <AdherezAuCollectif />
     </div>
   </div>
 </template>
