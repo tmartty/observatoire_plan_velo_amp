9648ac23cd357c8df6b4722e877afe3a0401e371
tomas <tomasmartty@gmail.com>
2024-02-09 21:00:17 +0100

    * lines individual page stats

--- components/content/Overview.vue
+++ components/content/Overview.vue
@@ -6,18 +6,18 @@
           class="mt-5 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x"
         >
           <div class="px-4 py-5 sm:p-6">
-            <dt class="mt-0 text-base font-normal text-gray-900">Distance</dt>
+            <dt class="mt-0 text-base font-normal text-gray-900">Réalisés</dt>
             <dd class="mt-1 pl-0 flex justify-between items-baseline md:block lg:flex">
               <div class="flex items-baseline text-2xl font-semibold" :style="`color: ${voie.color}`">
-                {{ Math.round(voie.distance / 1000) }}km
+                {{ Math.round(doneDistance / 1000) }}km
               </div>
             </dd>
           </div>
           <div class="px-4 py-5 sm:p-6">
-            <dt class="mt-0 text-base font-normal text-gray-900">Fréquentation</dt>
+            <dt class="mt-0 text-base font-normal text-gray-900">Manquants</dt>
             <dd class="mt-1 pl-0 flex justify-between items-baseline md:block lg:flex">
               <div class="flex items-baseline text-2xl font-semibold" :style="`color: ${voie.color}`">
-                {{ voie.trafic }}
+                {{ Math.round(missingDistance / 1000) }}km
               </div>
             </dd>
           </div>
@@ -25,7 +25,7 @@
             <dt class="mt-0 text-base font-normal text-gray-900">Avancement</dt>
             <dd class="mt-1 pl-0 flex justify-between items-baseline md:block lg:flex">
               <div class="flex items-baseline text-2xl font-semibold" :style="`color: ${voie.color}`">
-                {{ avancement }}%
+                {{ percentageComplete }}%
               </div>
             </dd>
           </div>
@@ -60,7 +60,10 @@ const { data: geojson } = await useAsyncData(`geojson-${path}`, () => {
 
 const features = geojson.value.features;
 
-const doneFeatures = features.filter(feature => feature.properties.status === 'done');
-const doneDistance = getDistance({ features: doneFeatures });
-const avancement = Math.round((doneDistance / voie.distance) * 100);
+const doneFeatures = features.filter(feature => feature.properties.status.includes('Réalisé'));
+const missingFeatures = features.filter(feature => feature.properties.status.includes('A réaliser'));
+
+const doneDistance = doneFeatures.reduce((acc, feature) => acc + feature.properties.calculated_length, 0);
+const missingDistance = missingFeatures.reduce((acc, feature) => acc + feature.properties.calculated_length, 0);
+const percentageComplete = Math.round((doneDistance / (doneDistance + missingDistance)) * 100);
 </script>
