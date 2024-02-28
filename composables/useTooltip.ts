e9719e6202cab8ccf7020d8eab587d14fe7a3eed
tomas <tomasmartty@gmail.com>
2024-02-28 19:11:02 +0100

    * fix

--- composables/useTooltip.ts
+++ composables/useTooltip.ts
@@ -77,19 +77,20 @@ export const useTooltip = () => {
     `;
   }
 
-  function getTooltipPerspective(properties: PerspectiveProperties) {
-    const color = getLineColor(properties.ligne);
-    return `
-      <div class="h-10 flex items-center not-prose text-black" style="background-color: ${color}">
-        <div class="p-2">
-          <a class='text-white font-bold text-lg hover:underline' href='/ligne-${properties.ligne}'>
-            Ligne ${properties.ligne}
-          </a>
-        </div>
-      </div>
-      <img src="${properties.imgUrl}" class='my-0'>
-    `;
-  }
+  // function getTooltipPerspective(properties: PerspectiveProperties) {
+  //   const color = getLineColor(properties.ligne);
+  //   return `
+  //     <div class="h-10 flex items-center not-prose text-black" style="background-color: ${color}">
+  //       <div class="p-2">
+  //         <a class='text-white font-bold text-lg hover:underline' href='/ligne-${properties.ligne}'>
+  //           Ligne ${properties.ligne}
+  //         </a>
+  //       </div>
+  //     </div>
+  //     <img src="${properties.imgUrl}" class='my-0'>
+  //   `;
+  // }
 
-  return { getTooltipHtml, getTooltipPerspective };
+  // return { getTooltipHtml, getTooltipPerspective };
+  return { getTooltipHtml };
 };
