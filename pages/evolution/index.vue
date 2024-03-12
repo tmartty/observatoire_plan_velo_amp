1f44960a8204f8df7266e29a32e5d52f4a27bca8
tomas <tomasmartty@gmail.com>
2024-03-12 14:22:15 +0100

    * change default value for evolution page

--- pages/evolution/index.vue
+++ pages/evolution/index.vue
@@ -33,8 +33,8 @@ definePageMeta({
 });
 
 const filters = ref([
-  { label: 'avant 2019', match: year => year < 2019, isChecked: false },
-  { label: '2019-2023', match: year => year >= 2019 && year < 2024, isChecked: true },
+  { label: 'avant 2019', match: year => year < 2019, isChecked: true },
+  { label: '2019-2023', match: year => year >= 2019 && year < 2024, isChecked: false },
   { label: '2024', match: year => year == 2024, isChecked: false }
 ]);
 
